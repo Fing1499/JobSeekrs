ba4c51f928e51e970ac3636fb0203cf09b165c43
fing1499 <findlayg@gmail.com>
2023-06-06 16:23:01 +0100

    adding styling

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 const passport = require('passport');
+const Application = require('../models/application');
 
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'DashBoard' });
+
+router.get('/', async function(req, res, next) {
+  try {
+    const applications = await Application.find({});
+    const user = req.user.id
+    res.render('applications/index', { title: 'DashBoard', applications, user });
+  } catch(err) {
+    console.log(err);
+    res.render('index', { errorMsg: err.message });
+    next();
+   } 
 });
 
 router.get('/auth/google', passport.authenticate(
@@ -16,7 +26,7 @@ router.get('/auth/google', passport.authenticate(
 router.get('/oauth2callback', passport.authenticate(
   'google',
   {
-    successRedirect: '/',
+    successRedirect: '/applications',
     failureRedirect: '/'
   }
 ));
